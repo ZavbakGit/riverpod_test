b5195d7ff55f3f2ee081a0064bdd345b079f6acd
ZavbakGit <ag@799000.ru>
2021-12-04 13:47:03 +1000

    add stateNotifire

--- app_riverpod_test/lib/main.dart
+++ app_riverpod_test/lib/main.dart
@@ -1,115 +1,127 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
+
+final valueProvider = Provider<int>((ref) {
+  return 56;
+});
+
+final counterStateProvider = StateProvider<int>((ref) {
+  return 0;
+});
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  // This widget is the root of your application.1
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Test riverpod',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: MyHomePageClock(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyhomePage extends ConsumerWidget {
+  const MyhomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final counter = ref.watch(counterStateProvider);
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    ref.listen<int>(counterStateProvider, (previous, current) {
+      // note: this callback executes when the provider value changes,
+      // not when the build method is called
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Value is $current')),
+      );
     });
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Consumer(
+          builder: (context, ref, child) {
+            return Text('Value: $counter');
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.add),
+        onPressed: () {
+          ref.read(counterStateProvider.state).state++;
+        },
+      ),
+    );
+  }
+}
+
+final clockProvider = StateNotifierProvider<Clock, DateTime>((ref) {
+  return Clock();
+});
+
+class Clock extends StateNotifier<DateTime> {
+  // 1. initialize with current time
+  Clock() : super(DateTime.now()) {
+    // 2. create a timer that fires every second
+    _timer = Timer.periodic(Duration(seconds: 1), (_) {
+      // 3. update the state with the current time
+      state = DateTime.now();
+    });
+  }
+
+  late final Timer _timer;
+
+  // 4. cancel the timer when finished
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+}
+
+class MyHomePageClock extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // this line is used to watch the provider's *state*
+    // to get an instance of the clock itself,
+    // call `ref.watch(clockProvider.notifier)`
+    final currentTime = ref.watch(clockProvider);
+    // format the time as `hh:mm:ss`
+    final timeFormatted = DateFormat.Hms().format(currentTime);
+    return Scaffold(
+      body: Center(
+        child: Text(
+          timeFormatted,
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ),
+    );
+  }
+}
+
+class MyhomePageStl extends StatelessWidget {
+  const MyhomePageStl({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Consumer(
+          builder: (context, ref, child) {
+            final value = ref.watch(valueProvider);
+            return Text('Value: $value');
+          },
+        ),
+      ),
     );
   }
 }
